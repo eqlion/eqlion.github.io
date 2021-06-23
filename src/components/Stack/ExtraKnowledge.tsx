14bc331a018a0ab208b9c71b36dbd98c61fe8bde
Nikolay Lapa <lapanicholas@gmail.com>
2021-06-23 11:56:40 +0300

    Small fixes

--- src/components/Stack/ExtraKnowledge.tsx
+++ src/components/Stack/ExtraKnowledge.tsx
@@ -5,6 +5,7 @@ import Container from "../Container";
 import Text from "../Text";
 
 import { LanguageContext } from "../../util/language";
+import { StyleSheet } from "react-native";
 
 const TITLE = {
     en: "Extra skills",
@@ -15,12 +16,14 @@ const SKILLS = {
     ru: [
         "Русский: носитель",
         "Английский: C1, формальная/неформальная речь, тех документация, читаю/пишу/говорю",
+        "Нативные библиотеки: понимаю Java, Kotlin, Swift; частично перерабатывал нативные библиотеки для RN для работы с новыми версиями",
         "LaTeX",
         "Python: всякая математика (numpy, matplotlib), чуть-чуть Flask, совсем чуть-чуть Django",
     ],
     en: [
         "Russian: native speaker",
         "English: C1, formal/informal, tech docs, can speak/write/read",
+        "Native libraries: I understand Java, Kotlin, Swift; have partially rewritten native libraries for RN to be able to work with new version",
         "LaTeX",
         "Python: maths applications (numpy, matplotlib), bits of Flask, miniscule bits of Django",
     ],
