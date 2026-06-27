1c4bf43aeee1950b612f470ca9684742def2d921
Nikolay Lapa <lapanicholas@gmail.com>
2026-06-27 23:37:07 +0700

    fix(layout): 🐛 stabilize initial width

--- src/hooks/useIsPhone.ts
+++ src/hooks/useIsPhone.ts
@@ -2,5 +2,8 @@ import { useWindowDimensions } from 'react-native';
 
 export const useIsPhone = () => {
     const { width } = useWindowDimensions();
+    if (!width) {
+        return false;
+    }
     return width < 769;
 };
