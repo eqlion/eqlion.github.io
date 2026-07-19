6cd4edcc0af385d98c9dbce94648c44eeca36bfb
Nikolay Lapa <lapanicholas@gmail.com>
2026-07-19 23:05:07 +0700

    feat(site): 🎸 apply Horizon theme and heading links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,29 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import { unified } from '@astrojs/markdown-remark';
 import sitemap from '@astrojs/sitemap';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+
+const headingLinkPlugin = [
+    rehypeAutolinkHeadings,
+    {
+        behavior: 'wrap',
+        properties: { className: ['heading-link'] },
+        test: ['h2', 'h3'],
+    },
+];
 
 export default defineConfig({
     site: 'https://eqlion.github.io',
     output: 'static',
     integrations: [mdx(), sitemap()],
     markdown: {
+        processor: unified({
+            rehypePlugins: [rehypeSlug, headingLinkPlugin],
+        }),
         shikiConfig: {
-            theme: 'github-dark',
+            theme: 'horizon',
         },
     },
 });
